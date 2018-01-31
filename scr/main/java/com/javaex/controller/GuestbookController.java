e16e81b84599fa8387276349fc910fb623c705c3
BIT-USER <BIT-USER@DESKTOP-2H6GGS7>
2018-01-31 17:26:25 +0900

    guestbook3 완료

---
+++ scr/main/java/com/javaex/controller/GuestbookController.java
@@ -0,0 +1,56 @@
+package com.javaex.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.dao.GuestBookDao;
+import com.javaex.vo.GuestBookVo;
+
+@Controller
+public class GuestbookController {
+	
+	@Autowired
+	private GuestBookDao guestbookDao;
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String list(Model model) {
+		System.out.println("list진입");
+		
+		List<GuestBookVo> list = guestbookDao.getList();
+		model.addAttribute("list", list);
+		
+		return "list";
+	}
+	
+	@RequestMapping(value = "/write", method = RequestMethod.GET)
+	public String write(@ModelAttribute GuestBookVo guestbookVo) {
+		
+		System.out.println(guestbookVo.toString());
+		
+		guestbookDao.insert(guestbookVo);
+		
+		return "redirect:list";
+	}
+	
+	@RequestMapping(value = "/deleteform", method = RequestMethod.GET)
+	public String deleteform() {
+		System.out.println("deleteform 진입");
+		
+		return "deleteform";
+	}
+	
+	@RequestMapping(value = "/delete", method = RequestMethod.GET)
+	public String delete(@ModelAttribute GuestBookVo guestbookVo) {
+		
+		guestbookDao.delete(guestbookVo.getNo(), guestbookVo.getPassword());
+		
+		return "redirect:list";
+	}
+	
+}
